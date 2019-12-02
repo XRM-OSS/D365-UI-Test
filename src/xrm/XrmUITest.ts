dc0c2efc6a24eeb53572e43b0955194df0c93708
Florian Kroenert <Florian.Kroenert@gmx.de>
2019-12-02 15:51:01 +0100

    Upgrade to ES2018

--- src/xrm/XrmUITest.ts
+++ src/xrm/XrmUITest.ts
@@ -105,9 +105,13 @@ export class XrmUiTest {
         return this.browser;
     }
 
-    open = async (url: string, extendedProperties?: { appId?: string; userName?: string; password?: string}) => {
+    /**
+     * @param { String } url Url of your D365 organization
+     * @param { Object } extendedProperties Options for logging in. User name and password are required. If you have a custom authentication page, you should pass userNameFieldSelector if user name has to be reentered and passwordFieldSelector for password entry. These are css selectors for the inputs.
+     */
+    open = async (url: string, extendedProperties: { appId?: string; userName: string; password: string, userNameFieldSelector?: string; passwordFieldSelector?: string }) => {
         this._crmUrl = url;
-        this._appId = extendedProperties && extendedProperties.appId;
+        this._appId = extendedProperties.appId;
 
         this._page = await this.browser.newPage();
 
@@ -119,41 +123,47 @@ export class XrmUiTest {
             this.page.waitForNavigation({ waitUntil: "networkidle0" })
         ]);
 
-        if (extendedProperties && extendedProperties.userName && extendedProperties.password) {
-            console.log(url);
-            console.log(extendedProperties.userName);
+        const userName = await this.page.waitForSelector("#i0116");
+        await userName.type(extendedProperties.userName);
 
-            const userName = await this.page.waitForSelector("#i0116");
-            await userName.type(extendedProperties.userName);
+        await this.page.waitFor(1000);
+        await userName.press("Enter");
+        await this.page.waitFor(1000);
 
-            await this.page.waitFor(1000);
-            await userName.press("Enter");
-            await this.page.waitFor(1000);
+        const password = await this.page.$("#i0118");
 
-            let password = await this.page.$("#i0118");
+        // For non online authentification, wait for custom login page to settle
+        if (!password) {
+            await this.page.waitForNavigation({ waitUntil: "networkidle0" });
+            console.log(`No online auth, handling custom auth. If nothing happens, please specify passwordFieldSelector and optionally userNameFieldSelector.`);
 
-            // For non online authentification, wait for custom login page to settle
-            if (!password) {
-                await this.page.waitForNavigation({ waitUntil: "networkidle0" });
-                password = await this.page.$("#passwordInput");
-
-                await password.type(extendedProperties.password);
-                await password.press("Enter");
-            }
-            else {
-                // For some reason we need the else in here, without it errors will occur
-                await password.type(extendedProperties.password);
-                await password.press("Enter");
+            if (extendedProperties.userNameFieldSelector) {
+                console.log("Waiting for user name field: " + extendedProperties.userNameFieldSelector);
+                const userNameField = await this.page.waitFor(extendedProperties.userNameFieldSelector);
+                await userNameField.type(extendedProperties.userName);
             }
 
-            const remember = await this.page.waitForSelector("#idBtn_Back");
+            if (extendedProperties.passwordFieldSelector) {
+                console.log("Waiting for password field: " + extendedProperties.passwordFieldSelector);
+                const passwordInput = await this.page.waitFor(extendedProperties.passwordFieldSelector);
 
-            await Promise.all([
-                this.page.waitForNavigation({ waitUntil: "networkidle0" }),
-                this.page.waitForNavigation({ waitUntil: "load" }),
-                remember.click()
-            ]);
+                await passwordInput.type(extendedProperties.password);
+                await passwordInput.press("Enter");
+            }
         }
+        else {
+            // For some reason we need the else in here, without it errors will occur
+            await password.type(extendedProperties.password);
+            await password.press("Enter");
+        }
+
+        const remember = await this.page.waitForSelector("#idBtn_Back");
+
+        await Promise.all([
+            this.page.waitForNavigation({ waitUntil: "networkidle0" }),
+            this.page.waitForNavigation({ waitUntil: "load" }),
+            remember.click()
+        ]);
 
         return this.page;
     }
