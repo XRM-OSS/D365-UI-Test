407bb9fcaf8c94337d73cd6acfce9d8b92f4129f
Florian Kroenert <Florian.Kroenert@gmx.de>
2020-03-02 11:10:21 +0100

    Fixes for using SSO

--- src/xrm/XrmUITest.ts
+++ src/xrm/XrmUITest.ts
@@ -10,6 +10,36 @@ import { Button } from "./Button";
 import { Tab } from "./Tab";
 
 /**
+ * Parameters for opening Dynamics
+ */
+export interface OpenProperties {
+    /**
+     * ID of the Dynamics app to open after login
+     */
+    appId?: string;
+
+    /**
+     * Username / email to use for logging in. If left out, it is assumed that CRM does not need explicit authentication (i.e. SSO)
+     */
+    userName?: string;
+
+    /**
+     * Password to use for logging in. If left out, SSO is assumed.
+     */
+    password?: string;
+
+    /**
+     * CSS selector for typing user name / email on login page. Needed for custom authentication pages of ADFS only.
+     */
+    userNameFieldSelector?: string;
+
+    /**
+     * CSS selector for typing password on login page. Needed for custom authentication pages of ADFS only.
+     */
+    passwordFieldSelector?: string;
+}
+
+/**
  * Main class for testing in D365
  */
 export class XrmUiTest {
@@ -180,7 +210,9 @@ export class XrmUiTest {
         // These URLs take sometimes more than 2 minutes to load, just abort them
         const ignoreUrlPaths = [
             "https://browser.pipe.aria.microsoft.com/Collector/3.0/?qsp=true&content-type=application%2Fbond-compact-binary&client-id=NO_AUTH",
-            "https://dc.services.visualstudio.com/v2/track"
+            "https://dc.services.visualstudio.com/v2/track",
+            "https://graph.windows.net/me?api-version=",
+            "https://loki.delve.office.com/api/v1/configuration/Dynamics365UCI/"
         ];
 
         page.on("request", req => {
@@ -199,7 +231,7 @@ export class XrmUiTest {
      * @param { Object } extendedProperties Options for logging in. User name and password are required. If you have a custom authentication page, you should pass userNameFieldSelector if user name has to be reentered and passwordFieldSelector for password entry. These are css selectors for the inputs.
      * @returns {puppeteer.Page} The page in which D365 was opened
      */
-    open = async (url: string, extendedProperties: { appId?: string; userName: string; password: string, userNameFieldSelector?: string; passwordFieldSelector?: string }) => {
+    open = async (url: string, extendedProperties: OpenProperties) => {
         this._crmUrl = url;
         this._appId = extendedProperties.appId;
 
@@ -216,34 +248,14 @@ export class XrmUiTest {
             this.page.waitForNavigation({ waitUntil: "networkidle0" })
         ]);
 
-        await this.enterUserName(extendedProperties);
-        await this.enterPassword(extendedProperties);
-        await this.dontRememberLogin();
+        if (extendedProperties.userName) {
+            await this.enterUserName(extendedProperties);
+        }
 
-        return this.page;
-    }
-
-    /**
-     * @param { String } url Url of your on premise D365 organization. For now, single sign on is expected.
-     * @param { Object } extendedProperties Options for logging in. User name and password are required. If you have a custom authentication page, you should pass userNameFieldSelector if user name has to be reentered and passwordFieldSelector for password entry. These are css selectors for the inputs.
-     * @returns {puppeteer.Page} The page in which D365 was opened
-     */
-    openOnPremise = async (url: string, extendedProperties: { appId?: string; }) => {
-        this._crmUrl = url;
-        this._appId = extendedProperties.appId;
-
-        this._page = await this.browser.newPage();
-
-        // Work around issues with linux user agents
-        this.page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chromium/60.0.3112.78 Safari/537.36");
-
-        // Register ignore URLs that sometimes cause trouble with timeouts
-        await this.registerIgnoreUrls(this.page);
-
-        await Promise.all([
-            this.page.goto(url, { waitUntil: "load" }),
-            this.page.waitForNavigation({ waitUntil: "networkidle0" })
-        ]);
+        if (extendedProperties.password) {
+            await this.enterPassword(extendedProperties);
+            await this.dontRememberLogin();
+        }
 
         await Promise.race([this.page.waitFor("#TabAppSwitcherNode"), this.page.waitFor("button[data-id='officewaffle']")]);
 
@@ -258,12 +270,11 @@ export class XrmUiTest {
     }
 
     private async dontRememberLogin() {
-        const remember = await this.page.waitForSelector("#idBtn_Back");
-        await remember.click();
-        return Promise.race([this.page.waitFor("#TabAppSwitcherNode"), this.page.waitFor("button[data-id='officewaffle']")]);
+        const remember = await this.page.waitForSelector("#idBtn_Back", {  });
+        return remember.click();
     }
 
-    private async enterPassword(extendedProperties: { appId?: string; userName: string; password: string; userNameFieldSelector?: string; passwordFieldSelector?: string; }) {
+    private async enterPassword(extendedProperties: OpenProperties) {
         const password = await this.page.$("#i0118");
         // For non online authentification, wait for custom login page to settle
         if (!password) {
@@ -288,7 +299,7 @@ export class XrmUiTest {
         }
     }
 
-    private async enterUserName(extendedProperties: { appId?: string; userName: string; password: string; userNameFieldSelector?: string; passwordFieldSelector?: string; }) {
+    private async enterUserName(extendedProperties: OpenProperties) {
         const userName = await this.page.waitForSelector("#i0116");
         await userName.type(extendedProperties.userName);
         await this.page.waitFor(1000);
