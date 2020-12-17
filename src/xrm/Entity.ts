6605e7234aff552ef6cfb6e56ef758bf21348c29
Florian Krönert <Florian.Kroenert@silflo.de>
2020-12-17 07:26:43 +0100

    Updated docs

--- src/xrm/Entity.ts
+++ src/xrm/Entity.ts
@@ -33,7 +33,7 @@ export class Entity {
 
     /**
      * Refreshes the current form record
-     * 
+     *
      * @param saveData Whether to save any unsubmitted data
      */
     refresh = async (saveData: boolean) => {
